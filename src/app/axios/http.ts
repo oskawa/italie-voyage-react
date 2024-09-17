86dfa6009c429d121288158cee073470e79669de
Maxime <maxime.eloir@agenceseize.fr>
2024-09-17 20:57:47 +0200

    Generate Slug V2

--- src/app/axios/http.ts
+++ src/app/axios/http.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
+import https from "https";
 
 const http = axios.create({
   baseURL: 'https://voyageapi.maxime-eloir.fr/wp-json/voyage-api/v1/',
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false, // Ignore self-signed certs
+  }),
 })
 
 export default http
