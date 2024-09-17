a01bfdd15857b1c3b460c26116225cb599b6cf64
Maxime <maxime.eloir@agenceseize.fr>
2024-09-17 19:54:40 +0200

    https api

--- src/app/axios/http.ts
+++ src/app/axios/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: 'http://voyageapi.maxime-eloir.fr/wp-json/voyage-api/v1/',
+  baseURL: 'https://voyageapi.maxime-eloir.fr/wp-json/voyage-api/v1/',
 })
 
 export default http
